74499bd6566c18f54d0477af0592316598ad6a50
deekshith-31123 <patnanasaideekshith@gmail.com>
2024-11-08 13:39:11 +0530

    Added Hackerrank solutions

---
+++ Hackerrank/Tag-Content-Extractor.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution{
+    public static void main(String[] args){
+        
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        String regex = "(<[^>]*>)";    
+        Pattern p = Pattern.compile(regex);
+        
+        while(testCases>0)
+        {
+            String line = in.nextLine();
+            
+              //Write your code here
+            Matcher m = p.matcher(line);
+            String prevTag = null;
+            int prevTagPos = -1;
+            boolean flag = false;
+            while(m.find())
+            {
+                String tag = line.substring(m.start(),m.end());
+                if(tag.charAt(1)!='/')
+                {
+                    prevTag = tag;
+                    prevTagPos = m.end();
+                }
+                else if (prevTag!=null && tag.charAt(1) == '/')
+                {
+                    if(prevTag.substring(1).equals(tag.substring(2)) && prevTag.length()>2 && m.start()>prevTagPos+1)
+                    {
+                        System.out.println(line.substring(prevTagPos,m.start()));
+                        flag = true;
+                    }
+                   
+                    prevTag = null;
+                }
+                
+            }
+            if(flag)
+            {
+                System.out.print("");
+            }
+            else
+            {
+                System.out.print("None\n");
+            }
+            testCases--;
+        }
+    }
+}
